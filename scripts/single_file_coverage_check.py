60c8bae991fd448729c6f6da43096289b159dca7
ori <oribarilan@gmail.com>
2023-09-02 21:48:47 +0300

    Update single_file_coverage_check.py

--- scripts/single_file_coverage_check.py
+++ scripts/single_file_coverage_check.py
@@ -2,6 +2,9 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 import sys
 
+# Define the threshold (X) for line-rate
+threshold = 0.90  # Adjust as needed
+
 # Get the directory of the script (where the script is located)
 script_dir = Path(__file__).resolve().parent
 
@@ -10,10 +13,8 @@ coverage_file_path = script_dir.parent / "coverage.xml"
 
 # Check if the coverage.xml file exists
 if not coverage_file_path.exists():
-    print(f"Coverage report not found at {coverage_file_path}, failing the workflow")
+    print("Coverage report not found, failing the workflow")
     sys.exit(1)
-else:
-    print(f"Coverage report found at {coverage_file_path}")
 
 tree = ET.parse(coverage_file_path)
 root = tree.getroot()
@@ -22,13 +23,13 @@ failed_files = []
 
 for class_element in root.findall('.//class[@filename]'):
     filename = class_element.get('filename')
-    line_rate = float(class_element.find('lines').get('line-rate'))
+    line_rate = float(class_element.get('line-rate'))
 
-    if line_rate < 0.90:  # Individual threshold per file (adjust as needed)
+    if line_rate < threshold:
         failed_files.append((filename, line_rate))
 
 if failed_files:
     print('The following files have coverage below the threshold:')
     for filename, line_rate in failed_files:
-        print(f'- {filename}: {line_rate * 100:.2f}%')  # Convert line_rate to percentage
+        print(f'- {filename}: {line_rate * 100:.2f}% coverage')  # Convert line_rate to percentage
     sys.exit(1)
