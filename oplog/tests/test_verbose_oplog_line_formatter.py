64b4376c366f95cd1d66525f8af259b020d027f8
Ori Bar-ilan <oribarilan@gmail.com>
2023-09-19 14:19:04 +0300

    csv formatter escaping (#3)
    * Adding escaping and tests
    
    * adding tests
    
    * linting
    
    * tests for coverage
    
    * increasting coverage

---
+++ oplog/tests/test_verbose_oplog_line_formatter.py
@@ -0,0 +1,75 @@
+import logging
+from unittest.mock import Mock
+
+from oplog import Operation
+from oplog.exceptions import LogRecordMissingOperationException
+from oplog.formatters import VerboseOplogLineFormatter
+from oplog.tests.logged_test_case import OpLogTestCase
+
+
+class VerboseOplogLineFormatterTestException(Exception):
+    pass
+
+
+class TestVerboseOplogLineFormatter(OpLogTestCase):
+    def test_format_operationWithCustomProperties(self):
+        # arrange
+        op_name = "test_operation"
+        custom_prop_name = "some_custom_prop"
+        custom_prop_value = "some_custom_value"
+        mock_record = Mock(spec=logging.LogRecord)
+        with Operation(name=op_name) as op:
+            op.add(custom_prop_name, custom_prop_value)
+        mock_record.oplog = op
+        formatter = VerboseOplogLineFormatter()
+
+        # act
+        log_line = formatter.format(record=mock_record)
+
+        # assert
+        self.assertIn(custom_prop_name, log_line)
+        self.assertIn(custom_prop_value, log_line)
+
+    def test_format_operationWithGlobalProperties(self):
+        # arrange
+        op_name = "test_operation"
+        global_prop_name = "some_global_prop"
+        global_prop_value = "some_global_value"
+        mock_record = Mock(spec=logging.LogRecord)
+        with Operation(name=op_name) as op:
+            op.add_global(global_prop_name, global_prop_value)
+        mock_record.oplog = op
+        formatter = VerboseOplogLineFormatter()
+
+        # act
+        log_line = formatter.format(record=mock_record)
+
+        # assert
+        self.assertIn(global_prop_name, log_line)
+        self.assertIn(global_prop_value, log_line)
+
+    def test_format_operationFailure(self):
+        # arrange
+        op_name = "test_operation"
+        error_msg = "some error message"
+        mock_record = Mock(spec=logging.LogRecord)
+        with Operation(name=op_name, suppress=True) as op:
+            raise VerboseOplogLineFormatterTestException(error_msg)
+        mock_record.oplog = op
+        formatter = VerboseOplogLineFormatter()
+
+        # act
+        log_line = formatter.format(record=mock_record)
+
+        # assert
+        self.assertIn(VerboseOplogLineFormatterTestException.__name__, log_line)
+        self.assertIn(error_msg, log_line)
+
+    def test_format_logRecordIsNotOplog(self):
+        # arrange
+        mock_record = Mock(spec=logging.LogRecord)
+        formatter = VerboseOplogLineFormatter()
+
+        # act
+        with self.assertRaises(LogRecordMissingOperationException):
+            formatter.format(record=mock_record)
